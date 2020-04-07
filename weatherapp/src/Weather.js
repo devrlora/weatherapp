3339ffbd15322a6e86ceeee69cd1076600c7db75
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-07 16:33:03 -0400

    add Days class component to file

---
+++ weatherapp/src/Weather.js
@@ -0,0 +1,21 @@
+// import React, {Component } from 'react';
+
+// // class Weather extends Component {
+//     constructor(){
+//         super(props)
+//         this.state = {
+//             day: 'Sunday',
+//             min: 0,
+//             max: 0
+//           }
+//     }
+//     render(){
+//         return (
+//             <div>
+//                <h1>weather</h1>
+//             </div>
+//         )
+//     }
+  
+// }
+// export default Weather;
