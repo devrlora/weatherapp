f3f463801c958e28627a3e6163174c0444a37924
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-07 15:02:17 -0400

    commit project

---
+++ weatherapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Days from './Days'
+
+function App() {
+  return (
+    <div className="App">
+      <Days />
+    </div>
+  );
+}
+
+export default App;
